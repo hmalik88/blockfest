422fa3f27b79978efc4002e40ebadbd3e068dc9b
hmalik88 <hbmalik88@gmail.com>
2019-08-30 20:50:29 -0400

    top-portion of page in progress

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import NavBar from '../components/NavBar'
 import FAQ from '../components/FAQ'
+import blockFestLogo from '../assets/blockfest2019.svg';
+import blockFestText from '../assets/blockfesttext.svg';
 import '../scss/Home.scss';
 
 class Home extends React.Component {
@@ -15,12 +17,26 @@ class Home extends React.Component {
     root.classList.remove('home-root');
   }
 
+
+
   render() {
     return(
       <>
-        <NavBar />
-        Hello World!
-        <FAQ />
+        <img className='blockLogo' src={blockFestLogo} alt='' />
+        <img className='blockText' src={blockFestText} alt='' />
+        <div className='headline'>NYC's premier blockchain hackathon hosted by BlockFest Group</div>
+        <div className='dlt-pill'>
+          <div className='date-text'>December 7-8, 2019</div>
+          <div className='location'>
+            <div className='location-text'>New York City, NY</div>
+              <div className='apply'>
+                <div className='apply-text'>Apply to Hack</div>
+                <div className='sponsor'>
+                  Become a sponsor
+              </div>
+            </div>
+          </div>
+        </div>
       </>
       )
   }
