d67862b8270bb3bd32363b749c9703551ad35a1a
hmalik88 <hbmalik88@gmail.com>
2019-08-31 18:40:39 -0400

    about section in

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -20,8 +20,6 @@ class Home extends React.Component {
     root.classList.remove('home-root');
   }
 
-
-
   render() {
     return(
       <>
@@ -43,6 +41,14 @@ class Home extends React.Component {
           </div>
         </div>
         <img className='skyline' src={skyline} alt='' />
+        <div className='about-section'>
+          <div className='about-headline'>
+            <h1 className='headline-text'>Build the Decentralized Future</h1>
+          </div>
+          <div className='about-text'>
+            <p className='about-paragraph'>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; The leading minds in the cryptocurrency space are joining over 400 hackers from around the world to collaborate together on decentralized applications using Ethereum. With an exponential increase in adoption, developer interest, and other teams building on top of Ethereum, there has never been a better time to be a part of the community, building Web 3.0.</p>
+          </div>
+        </div>
       </>
       )
   }
