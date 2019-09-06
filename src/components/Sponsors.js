69e03ba15eda2ae00a54bca1ea5515089321cebc
hmalik88 <hbmalik88@gmail.com>
2019-09-05 22:44:26 -0400

    added in sponsor assets, basic grid/containers in place for sponsors

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import sponsor from '../assets/sponsors.svg';
+import '../scss/Sponsors.scss';
 
 function Sponsors() {
   return(
       <>
+        <div className='sponsor-svg-div'>
+          <img src={sponsor} className='sponsor-svg' alt='' />
+        </div>
+        <div className='tier-container'>
+
+        </div>
       </>
     )
 }
