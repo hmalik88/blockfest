192711a0222fcc629e64c4e34d8387cc096966fc
hmalik88 <hbmalik88@gmail.com>
2019-09-21 23:22:12 -0400

    fixed padding in all sections, everything is properly sized, added media queries and redid venue section to be properly responsive

--- src/components/Venue.js
+++ src/components/Venue.js
@@ -16,18 +16,24 @@ function Venue() {
             <img src={microsoftHQ} alt='' className='hqPic' />
           </div>
           <div className="location-details-container">
-            <img src={microsoftLogo} alt='' className='microsoft-logo' />
-            <h1 className='microsoft-headline'>
-              Microsoft HQ NYC
-            </h1>
-            <p className="microsoft-headline-text">
-              11 Times Sq. New York, NY 10036
-            </p>
-            <a href='https://www.google.com/maps/dir/?api=1&origin=Your+location&destination=Microsoft+Times+Square+New+York+NY&travelmode=transit' target="_blank" rel="noopener noreferrer">
-              <button className='directions'>
-                Click here for directions
-              </button>
-            </a>
+            <div className='microsoft-details-container'>
+              <div className="microsoft-logo-container">
+                <div className='logo-inner-container'>
+                  <img src={microsoftLogo} alt='' className='microsoft-logo' />
+                  <h1 className='microsoft-headline'>
+                    Microsoft HQ NYC
+                  </h1>
+                </div>
+                <p className="microsoft-headline-text">
+                11 Times Sq. New York, NY 10036
+                </p>
+              </div>
+              <a href='https://www.google.com/maps/dir/?api=1&origin=Your+location&destination=Microsoft+Times+Square+New+York+NY&travelmode=transit' target="_blank" rel="noopener noreferrer">
+                <button className='directions'>
+                  Click here for directions
+                </button>
+              </a>
+            </div>
           </div>
         </div>
       </div>
